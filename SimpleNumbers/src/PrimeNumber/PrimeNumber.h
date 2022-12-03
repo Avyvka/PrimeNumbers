e6de2b1af99c49dac639607440991cdb8e6a15b5
Avyvka <lamonov.yura@gmail.com>
2022-12-03 17:28:05 +0300

    update comments

--- SimpleNumbers/src/PrimeNumber/PrimeNumber.h
+++ SimpleNumbers/src/PrimeNumber/PrimeNumber.h
@@ -6,32 +6,32 @@
 class PrimeNumber
 {
 public:
-	/// <summary>
-	/// Проверяет число на простое
-	/// </summary>
-	/// <param name="number">Проверяемое число</param>
-	/// <returns>true - Простое / false - Нет</returns>
-	bool is(unsigned int number);
+    /// <summary>
+    /// Проверяет число на простое
+    /// </summary>
+    /// <param name="number">- проверяемое число</param>
+    /// <returns>true - Простое / false - Нет</returns>
+    bool is(unsigned int number);
 
-	/// <summary>
-	/// Возвращает простые числа до некоторого числа
-	/// </summary>
-	/// <param name="last">Последнее число</param>
-	/// <returns>Массив простых чисел</returns>
-	std::vector<unsigned int> getUpTo(unsigned int last);
+    /// <summary>
+    /// Возвращает простые числа до некоторого числа
+    /// </summary>
+    /// <param name="last">- последнее число</param>
+    /// <returns>Массив простых чисел</returns>
+    std::vector<unsigned int> getUpTo(unsigned int last);
 
-	/// <summary>
-	/// Возвращает первые несколько простых чисел
-	/// </summary>
-	/// <param name="count">Количество простых чисел</param>
-	/// <returns>Массив простых чисел</returns>
-	std::vector<unsigned int> getFirsts(unsigned int count);
+    /// <summary>
+    /// Возвращает первые несколько простых чисел
+    /// </summary>
+    /// <param name="count">- количество простых чисел</param>
+    /// <returns>Массив простых чисел</returns>
+    std::vector<unsigned int> getFirsts(unsigned int count);
 
-	/// <summary>
-	/// Возвращает простое число по порядковому номеру
-	/// </summary>
-	/// <param name="n">Порядковый номер числа</param>
-	/// <returns>Простое число</returns>
-	unsigned int get(unsigned int n);
+    /// <summary>
+    /// Возвращает простое число по порядковому номеру
+    /// </summary>
+    /// <param name="n">- порядковый номер числа</param>
+    /// <returns>Простое число</returns>
+    unsigned int get(unsigned int n);
 };
 
--- SimpleNumbers/src/Test/Test.cpp
+++ SimpleNumbers/src/Test/Test.cpp
@@ -1,48 +1,48 @@
 #include "Test.h"
 
 /*
-		 n                         n
-	-----------  <  Count  <  -----------
-	ln(n) - 3/2               ln(n) - 1/2
+         n                         n
+    -----------  <  Count  <  -----------
+    ln(n) - 3/2               ln(n) - 1/2
 */
 bool Test::formulaTwoThree(int n, int count)
 {
-	double left = n / (ln(n) - 1.0 / 2);
-	double right = n / (ln(n) - 3.0 / 2);
+    double left = n / (ln(n) - 1.0 / 2);
+    double right = n / (ln(n) - 3.0 / 2);
 
-	bool result = left < count && count < right;
-	
-	std::cout << std::round(left) << " < ";
-	std::cout << count << " < ";
-	std::cout << std::round(right);
-	
-	std::string str = result ? "верно" : "не верно";
-	std::cout << " - " << str << "\n";
+    bool result = left < count && count < right;
+    
+    std::cout << std::round(left) << " < ";
+    std::cout << count << " < ";
+    std::cout << std::round(right);
+    
+    std::string str = result ? "верно" : "не верно";
+    std::cout << " - " << str << "\n";
 
-	return result;
+    return result;
 }
 
 /*
-	n( ln(n) + ln(ln(n)) - 3/2 )  <  number  <  n( ln(n) + ln(ln(n)) - 1/2 )
+    n( ln(n) + ln(ln(n)) - 3/2 )  <  number  <  n( ln(n) + ln(ln(n)) - 1/2 )
 */
 bool Test::formulaFour(int n, unsigned int number)
 {
-	double left = n * (ln(n) + ln(ln(n)) - 3.0 / 2);
-	double right = n * (ln(n) + ln(ln(n)) - 1.0 / 2);
+    double left = n * (ln(n) + ln(ln(n)) - 3.0 / 2);
+    double right = n * (ln(n) + ln(ln(n)) - 1.0 / 2);
 
-	bool result = left < number && number < right;
+    bool result = left < number && number < right;
 
-	std::cout << std::round(left) << " < ";
-	std::cout << number << " < ";
-	std::cout << std::round(right);
+    std::cout << std::round(left) << " < ";
+    std::cout << number << " < ";
+    std::cout << std::round(right);
 
-	std::string str = result ? "верно" : "не верно";
-	std::cout << " - " << str << "\n";
+    std::string str = result ? "верно" : "не верно";
+    std::cout << " - " << str << "\n";
 
-	return result;
+    return result;
 }
 
 double Test::ln(double number)
 {
-	return std::log(number);
+    return std::log(number);
 }
